3a7250e26d0c3701bb663f26a52a7d1cce78a67d
Mark <markfitz815@gmail.com>
2025-06-21 23:11:01 +0100

    Finish selectionSort

--- src/selection-sort.ts
+++ src/selection-sort.ts
@@ -4,19 +4,28 @@
 function selectionSort(unsortedArr: number[]) {
   let lowestNum: number | null = null;
   let lowestNumIndex: number | null = null;
-  // Loop through array
+  // Loop through all items in array
   for (let i = 0; i < unsortedArr.length; i++) {
-    if (lowestNum === null || lowestNum > unsortedArr[i]) {
-      lowestNum = unsortedArr[i];
-      lowestNumIndex = i;
+    // Loop through remaining items during each iteration
+    for (let j = 0 + i; j < unsortedArr.length; j++) {
+      if (lowestNum === null || lowestNum > unsortedArr[j]) {
+        lowestNum = unsortedArr[j];
+        lowestNumIndex = j;
+      }
     }
-  }
-  //   If we found a low number then remove it
-  if (lowestNumIndex && lowestNum) {
-    unsortedArr.slice(lowestNumIndex, 1);
-    unsortedArr.unshift(lowestNum);
-    lowestNum = null;
-    lowestNumIndex = null;
+
+    //   If we found a low number then remove it
+    if (lowestNumIndex && lowestNum) {
+      // Move everything between start and moved number forward one space
+      for (let n = lowestNumIndex; n > i; n--) {
+        unsortedArr.splice(n, 1, unsortedArr[n - 1]);
+      }
+      // Move lowest number to the lowest unsorted position
+      unsortedArr.splice(i, 1, lowestNum);
+      // Reset lowestNum vals
+      lowestNum = null;
+      lowestNumIndex = null;
+    }
   }
 
   return unsortedArr;
