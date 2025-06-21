190d3c8cbf6e7078a0d9316f259c13a98c83d3d8
Mark <markfitz815@gmail.com>
2025-06-21 12:51:47 +0100

    Implement bubble sort (w/o optimisations)

--- src/bubble-sort.ts
+++ src/bubble-sort.ts
@@ -1,6 +1,22 @@
+// Go through the array, one value at a time.
+// For each value, compare the value with the next value.
+// If the value is higher than the next one, swap the values so that the highest value comes last.
+// Go through the array as many times as there are values in the array.
 function bubbleSort(numArr: number[]): number[] {
-  console.log(numArr);
-  return [];
+  let lastSwapPos = numArr.length - 1;
+  // Go through all items
+  for (let i = 0; i < numArr.length; i++) {
+    // One less item to check with each loop
+    for (let j = 0; j < lastSwapPos; j++) {
+      if (numArr[j] > numArr[j + 1]) {
+        const tempFirst = numArr[j];
+        numArr[j] = numArr[j + 1];
+        numArr[j + 1] = tempFirst;
+      }
+    }
+    lastSwapPos--;
+  }
+  return numArr;
 }
 
-bubbleSort([5, 10, 3, 1, 2, 20, 8]);
+console.log(bubbleSort([5, 40, 10, 3, 1, 2, 20, 8]));
